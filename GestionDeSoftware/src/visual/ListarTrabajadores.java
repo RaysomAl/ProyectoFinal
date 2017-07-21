be4180ffd18766b8189fb242dbdd9a45bd9efaa5
pamelaovalles <pamela.ovalles.18@gmail.com>
2017-07-21 09:24:32 -0400

    haciendo cambios

--- GestionDeSoftware/src/visual/ListarTrabajadores.java
+++ GestionDeSoftware/src/visual/ListarTrabajadores.java
@@ -9,6 +9,11 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import java.awt.Toolkit;
 import javax.swing.border.TitledBorder;
+import javax.swing.JLabel;
+import javax.swing.JComboBox;
+import javax.swing.UIManager;
+import java.awt.Color;
+import javax.swing.JFormattedTextField;
 
 public class ListarTrabajadores extends JDialog {
 
@@ -42,26 +47,42 @@ public class ListarTrabajadores extends JDialog {
 		
 		JPanel ListaTrabajadores = new JPanel();
 		ListaTrabajadores.setBorder(new TitledBorder(null, "Lista de trabajadores", TitledBorder.CENTER, TitledBorder.TOP, null, null));
-		ListaTrabajadores.setBounds(12, 104, 691, 430);
+		ListaTrabajadores.setBounds(12, 82, 691, 452);
 		contentPanel.add(ListaTrabajadores);
 		
 		JPanel proyectosActivos = new JPanel();
 		proyectosActivos.setBorder(new TitledBorder(null, "Proyectos activos", TitledBorder.CENTER, TitledBorder.TOP, null, null));
-		proyectosActivos.setBounds(715, 36, 319, 498);
+		proyectosActivos.setBounds(715, 11, 319, 523);
 		contentPanel.add(proyectosActivos);
 		proyectosActivos.setLayout(null);
 		
 		JPanel FiltroTipo = new JPanel();
 		FiltroTipo.setBorder(new TitledBorder(null, "Filtro por Tipo", TitledBorder.CENTER, TitledBorder.TOP, null, null));
-		FiltroTipo.setBounds(12, 36, 329, 57);
+		FiltroTipo.setBounds(12, 11, 329, 60);
 		contentPanel.add(FiltroTipo);
 		FiltroTipo.setLayout(null);
 		
+		JLabel lblFiltro = new JLabel("Filtro:");
+		lblFiltro.setBounds(10, 24, 61, 14);
+		FiltroTipo.add(lblFiltro);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setBounds(81, 21, 198, 20);
+		FiltroTipo.add(comboBox);
+		
 		JPanel Busquedad = new JPanel();
-		Busquedad.setBorder(new TitledBorder(null, "Busquedad", TitledBorder.CENTER, TitledBorder.TOP, null, null));
-		Busquedad.setBounds(390, 36, 313, 57);
+		Busquedad.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "B\u00FAsqueda", TitledBorder.CENTER, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		Busquedad.setBounds(390, 11, 313, 60);
 		contentPanel.add(Busquedad);
 		Busquedad.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("C\u00E9dula:");
+		lblNewLabel.setBounds(10, 23, 59, 14);
+		Busquedad.add(lblNewLabel);
+		
+		JFormattedTextField formattedTextField = new JFormattedTextField();
+		formattedTextField.setBounds(67, 20, 222, 20);
+		Busquedad.add(formattedTextField);
 		{
 			JPanel buttonPane = new JPanel();
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
