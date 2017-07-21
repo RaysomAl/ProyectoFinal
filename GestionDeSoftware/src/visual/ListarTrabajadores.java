905988cf2e4a43395b1bc1b08fcaceb2f1272c0e
pamelaovalles <pamela.ovalles.18@gmail.com>
2017-07-21 09:19:22 -0400

    comenzando la ventana de listar

--- GestionDeSoftware/src/visual/ListarTrabajadores.java
+++ GestionDeSoftware/src/visual/ListarTrabajadores.java
@@ -7,6 +7,8 @@ import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.Toolkit;
+import javax.swing.border.TitledBorder;
 
 public class ListarTrabajadores extends JDialog {
 
@@ -29,11 +31,37 @@ public class ListarTrabajadores extends JDialog {
 	 * Create the dialog.
 	 */
 	public ListarTrabajadores() {
-		setBounds(100, 100, 450, 300);
+		setIconImage(Toolkit.getDefaultToolkit().getImage(ListarTrabajadores.class.getResource("/img/listar32.png")));
+		setTitle("Lista de Trabajadores");
+		setResizable(false);
+		setBounds(100, 100,  1050, 597);
 		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setLayout(new FlowLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		
+		JPanel ListaTrabajadores = new JPanel();
+		ListaTrabajadores.setBorder(new TitledBorder(null, "Lista de trabajadores", TitledBorder.CENTER, TitledBorder.TOP, null, null));
+		ListaTrabajadores.setBounds(12, 104, 691, 430);
+		contentPanel.add(ListaTrabajadores);
+		
+		JPanel proyectosActivos = new JPanel();
+		proyectosActivos.setBorder(new TitledBorder(null, "Proyectos activos", TitledBorder.CENTER, TitledBorder.TOP, null, null));
+		proyectosActivos.setBounds(715, 36, 319, 498);
+		contentPanel.add(proyectosActivos);
+		proyectosActivos.setLayout(null);
+		
+		JPanel FiltroTipo = new JPanel();
+		FiltroTipo.setBorder(new TitledBorder(null, "Filtro por Tipo", TitledBorder.CENTER, TitledBorder.TOP, null, null));
+		FiltroTipo.setBounds(12, 36, 329, 57);
+		contentPanel.add(FiltroTipo);
+		FiltroTipo.setLayout(null);
+		
+		JPanel Busquedad = new JPanel();
+		Busquedad.setBorder(new TitledBorder(null, "Busquedad", TitledBorder.CENTER, TitledBorder.TOP, null, null));
+		Busquedad.setBounds(390, 36, 313, 57);
+		contentPanel.add(Busquedad);
+		Busquedad.setLayout(null);
 		{
 			JPanel buttonPane = new JPanel();
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
@@ -51,5 +79,4 @@ public class ListarTrabajadores extends JDialog {
 			}
 		}
 	}
-
 }
